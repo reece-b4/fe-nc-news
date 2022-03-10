74ee6ae9e0f07704fd3a3601e96fa80a149e5588
reece-b4 <reecebalfour@gmail.com>
2022-03-10 10:18:12 +0000

    remove comment votes as not supported by backend

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -7,7 +7,8 @@ export default ({author, body, created_at, votes, article_id}) => {
     <p>{body}</p>
     <ExpandingWrapper>
         <div>
-    <Votes initialVotes={votes} article_id={article_id}/>
+    {/* <Votes initialVotes={votes} article_id={article_id}/> */}
+    {/* votes component is for article votes, backend doesn't support patch votes for comment votes? */}
     <p>{author}{created_at}</p>
     </div>
     </ExpandingWrapper>
