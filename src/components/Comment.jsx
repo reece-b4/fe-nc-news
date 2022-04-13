0521066e2e2cbbe14acbe27601140b2d1c3038a6
reece-b4 <reecebalfour@gmail.com>
2022-04-13 13:43:07 +0100

    change empty comment box alert to rendered text and comment deleted alert to fading popup

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,7 @@ import { UserContext } from "../contexts/User.Context"
 import { useContext } from 'react'
 
 
-export default ({author, body, created_at, comment_id, commentsList, setCommentsList, votes, article_id}) => {
+export default ({author, body, created_at, comment_id, commentsList, setCommentsList, votes, article_id, setDisplayPopup}) => {
     const {user} = useContext(UserContext)
     const formattedDate = new Date(created_at).toLocaleString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute:'2-digit'});
 
@@ -11,7 +11,7 @@ export default ({author, body, created_at, comment_id, commentsList, setComments
     <>
     <p className='commentBody'>{body}</p>
 
-    {user === author ? <DeleteCommentButton author={author} comment_id={comment_id} commentsList={commentsList} setCommentsList={setCommentsList}/>:null }
+    {user === author ? <DeleteCommentButton author={author} comment_id={comment_id} commentsList={commentsList} setCommentsList={setCommentsList} setDisplayPopup={setDisplayPopup}/>:null }
     
     <ExpandingWrapper>
         <div id='expandingWrapperDiv'>
